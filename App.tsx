7a3e32d600c01e4e58319f11507c7db7e57e6b2b
Oscar <oscarsandbergso@gmail.com>
2021-03-06 18:30:27 +0100

    UI kitten icons

--- App.tsx
+++ App.tsx
@@ -2,12 +2,26 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Navigation from "./src/navigation";
+import useCachedResources from "./src/hooks/useCachedResources";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
+import * as eva from "@eva-design/eva";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
-      <Navigation />
-      <StatusBar />
-    </SafeAreaProvider>
-  );
+  const isLoadingComplete = useCachedResources();
+  if (isLoadingComplete) {
+    return (
+      <>
+        <IconRegistry icons={EvaIconsPack} />
+        <ApplicationProvider {...eva} theme={eva.light}>
+          <SafeAreaProvider>
+            <Navigation />
+            <StatusBar />
+          </SafeAreaProvider>
+        </ApplicationProvider>
+      </>
+    );
+  } else {
+    return null;
+  }
 }
